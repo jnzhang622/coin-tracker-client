1b77736a2722ccfdb2f97d387cd7a46c3f3ceda5
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-07-25 19:13:35 -0400

    logout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/NavBar';
 import UserNavbar from './Components/UserNavBar';
 import SignUp from './Components/SignUp';
 import LogIn from './Components/LogIn';
+import LogOut from './Components/LogOut';
 import './App.css';
 
 
@@ -22,10 +23,10 @@ setUser = (user) => {
 }
 
   render(){
-    let navbar = <Navbar currentUser={this.state}/>
+    let navbar = <Navbar />
 
     if (this.state.currentUser) {
-      navbar = <UserNavbar currentUser={this.state}/>
+      navbar = <UserNavbar currentUser={this.state} setUser={this.setUser}/>
     }
     console.log(this.state)
     return (
@@ -37,6 +38,7 @@ setUser = (user) => {
         <Route exact path="/exchanges" component={CoinContainer}/>
         <Route exact path="/signup" render={(routerProps)=> <SignUp setUser={this.setUser} {...routerProps}/>}/>
         <Route exact path="/login" render={(routerProps)=> <LogIn setUser={this.setUser} {...routerProps}/>}/>
+        <Route exact path="/logout" render={(routerProps)=> <LogOut setUser={this.setUser} {...routerProps}/>}/>
        </Switch>
       </div>
     )
--- src/Components/LogOut.jsx
+++ src/Components/LogOut.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
 const LogOut = props => {
+  props.setUser(null)
 
+ console.log(props)
+  return (
+    <>
+    <h1>You have logged out!</h1>
+    </>
+  )
 }
+
+export default LogOut
