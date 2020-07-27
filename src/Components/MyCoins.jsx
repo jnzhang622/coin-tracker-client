0ae8c2c94b38e8969cd8e7456a884683dc3fbc37
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-07-27 10:18:13 -0400

    mycoins update

--- src/Components/MyCoins.jsx
+++ src/Components/MyCoins.jsx
@@ -3,9 +3,20 @@ import SearchBar from './SearchBar'
 import CoinCard from './CoinCard';
 
 class MyCoins extends React.Component {
+ state = {
+   my_coins: []
+ }
 
+ componentDidMount() { 
+   fetch("https://api.nomics.com/v1/currencies/ticker?key=demo-26240835858194712a4f8cc0dc635c7a&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR")
+     .then(response => response.json())
+     .then(data => {
+       console.log(data)
+        this.setState({my_coins: data})
+     })
+ }
   render(){
-    console.log(this.props)
+    console.log(this.state)
     if (this.props.currentUser) {
     return(
       <>
@@ -14,8 +25,7 @@ class MyCoins extends React.Component {
        <div className="cards">
        //we have to find get coins by symbol and then map
            {
-               this.props.currentUser[1].map(coin => {
-                   return <CoinCard key={coin.id} coin={coin}/> })
+
            }
        </div>
        </>
