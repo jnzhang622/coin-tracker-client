6f9e67ec4db1d9487b88e365c65cb0aa205d19ae
jnzhang622 <jnzhang622@gmail.com>
2020-07-27 14:57:23 -0400

    added more buttons to show page

--- src/Components/CoinContComp/CoinShowPage.jsx
+++ src/Components/CoinContComp/CoinShowPage.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 
 class CoinShowPage extends React.Component {
+
+    state = {
+        priceRenderChanges: "Daily"
+    }
+
+    handleChange = (e) => {
+        if (e.target.value === "Daily") {
+            this.setState({ priceRenderChanges: e.target.value })}
+        else if (e.target.value === "Weekly") {
+            this.setState({ priceRenderChanges: e.target.value })}
+        else if (e.target.value === "Monthly") {
+            this.setState({ priceRenderChanges: e.target.value })}
+        else if (e.target.value === "Yearly") {
+            this.setState({ priceRenderChanges: e.target.value })}
+        else if (e.target.value === "YearToDate") {
+            this.setState({ priceRenderChanges: e.target.value })}
+            console.log(this.state.priceRenderChanges)
+    }
+
     render() {
         let { name, logo_url, rank, 
             currency, symbol, price, 
@@ -8,7 +27,7 @@ class CoinShowPage extends React.Component {
             circulating_supply, max_supply, 
             market_cap, high, high_timestamp 
         } = this.props.coin[0]
-        console.log(this.props.coin[0])
+        
         return (
             <div>
                 {
@@ -32,9 +51,22 @@ class CoinShowPage extends React.Component {
                                 <a className="showPage__info">Rank: {rank}</a>
                                 <a className="showPage__info">High: {high}</a>
                                 <a className="showPage__info">High Timestamp: {high_timestamp}</a>
-
-                                <button name="cardDetails" onClick={this.props.handleChange}>Go Back</button>
-                                <button >Add to Watch</button>
+                                
+                                <div>
+                                    <button className="card_button" name="cardDetails" onClick={this.props.handleChange}>Go Back</button>
+                                    <button className="card_button">Post Comment</button>
+                                    <button className="card_button">Add to Watch</button>
+                                </div>
+                            </div>
+                            <div>
+                                <select onChange={this.handleChange} name="timeChange">
+                                    <option value="Daily"> Daily Change</option>
+                                    <option value="Weekly">Weekly Change</option>
+                                    <option value="Monthly">Monthly Change</option>
+                                    <option value="Yearly">Yearly Change</option>
+                                    <option value="YearToDate">Year to Date</option>
+                                </select>
+                                
                             </div>
                         </div>
                     </div>
