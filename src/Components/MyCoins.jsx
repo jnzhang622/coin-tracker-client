ef0ec9fc289ae04e15d49989cfdd246f86525105
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-07-28 22:54:15 -0400

    my coins tracking

--- src/Components/MyCoins.jsx
+++ src/Components/MyCoins.jsx
@@ -21,17 +21,6 @@ class MyCoins extends React.Component {
    }
  }
 
-componentDidUpdate(){
-    console.log(this.props.currentUser)
-    if (this.props.currentUser && this.props.currentUser[1].length > 0) {
-        let symbols = this.props.currentUser[1].map(coin => coin.symbol)
-    fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.REACT_APP_API_KEY}&ids=${symbols.join()}&interval=1d,30d&convert=USD`)
-      .then(response => response.json())
-      .then(data => {
-         this.setState({my_coins: data})
-      })
-    }
-  }
 
   render(){
     if (this.props.currentUser && this.state.my_coins.length > 0) {
@@ -48,10 +37,17 @@ componentDidUpdate(){
        </div>
        </>
     )
+  } else if (this.props.currentUser){
+    return(
+      <>
+      <h1>Welcome {this.props.currentUser[0].user_name}</h1>
+       <h1>Tracked coins will be displayed here...</h1>
+      </>
+    )
   } else {
     return(
       <>
-       <h1>Loading...</h1>
+       <h1>Please Sign up, or Log in to start tracking your favorite coins!</h1>
       </>
     )
   }
